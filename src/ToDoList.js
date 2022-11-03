1bc45d41c77d4215ae5d87d70a141cb3110a28ba
dkbosei00 <dannykosei00@gmail.com>
2022-11-03 20:08:48 +0000

    First Commit

---
+++ src/ToDoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToDo from './ToDo'
+
+export default function ToDoList({todos, toggleToDo}) {
+    
+  return (
+    todos.map(todo => {
+        return <ToDo key = {todo.id} toggleToDo = {toggleToDo} todo={todo} />
+    }
+
+    )
+
+  )
+}
